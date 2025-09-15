a1ba6424f9251a1311396569a6b7b8e53f7932eb
Ashok Kumar <ashok.kumar@gmail.com>
2025-09-15 11:50:38 +0530

    updated provider

--- enviroment/dev/provider.tf
+++ enviroment/dev/provider.tf
@@ -5,17 +5,19 @@ terraform {
       version = "4.41.0"
     }
   }
-  backend "azurerm" {
-    tenant_id            = "0cd94da3-6ea1-4939-8630-bd60916b12c2"
-    resource_group_name  = "dev-rg-ashok"
-    storage_account_name = "devaskstg001"
-    container_name       = "dev-container"
-    key                  = "dev1.tfstate"
-
+ backend "azurerm" {
+     tenant_id        = "ba464e9e-9154-4fc0-9582-760e55463849"  
+     subscription_id  = "cefa80e5-9af0-4e4a-9f43-e0a0491a5473" 
+     resource_group_name  = "dev-rg-ask01"
+     storage_account_name = "devstgask01"
+     container_name       = "tfstate-container"
+     key                  = "aks.tfstate"
+    
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = "f0ea4d91-8845-4cde-80fa-52ca5703a10a"
+  subscription_id = "cefa80e5-9af0-4e4a-9f43-e0a0491a5473"
+   resource_provider_registrations = "none"
 }
